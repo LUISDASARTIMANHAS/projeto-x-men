11a518b6c8bd343fdee962c3a0baef7bab4ad6d5
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-08 22:49:50 -0300

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 const personages = document.querySelectorAll(".personagem")
 const linkSrc = "/src/imagens/card-"
+const bacgroundAudio = new Audio("./src/X%20Men%20Evolution%201%C2%BA%20Abertura%20(128%20kbps).mp3")
+const playStationIntro = new Audio("./src/play%20station%20intro.mp3")
+const playStationSelecionar = new Audio("./src/audio%20play%20station%20Selecionar.mp3")
 
+playStationIntro.play()
+bacgroundAudio.play()
+bacgroundAudio.loop = true
+bacgroundAudio.volume = 0.05
 personages.forEach(personagem => {
     personagem.addEventListener("mouseenter", () => {
         aoPassarDoMouse(personagem)
@@ -18,7 +25,8 @@ function aoPassarDoMouse(personagem) {
     if (window.innerWidth < 620) {
         window.scrollTo({ top: 0, behavior: "smooth" })
     }
-    
+
+    playStationSelecionar.play()
     personagemSelec.classList.remove("selecionado")
     personagem.classList.add("selecionado")
     personagemGGIMG.src = link
