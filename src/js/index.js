55229a6dfaca2195bd7887e69b48f663a42b296b
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-08 22:06:50 -0300

    correção de bugs

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,16 @@ personages.forEach(personagem => {
 
 function aoPassarDoMouse(personagem) {
     const personagemSelec = document.querySelector(".selecionado")
-    const personagemGGIMG = document.querySelector("personagem-GG-img")
+    const personagemGGIMG = document.querySelector(".personagem-GG-img")
     const personagemID = personagem.attributes.id.value
     const nomePersonagem = document.getElementById("nomePersonagem")
     const descricaoPersonagem = document.getElementById("descricaoPersonagem")
     const link = linkSrc + personagemID + ".png"
 
-    console.log(link)
     if (window.innerWidth < 620) {
         window.scrollTo({ top: 0, behavior: "smooth" })
     }
+    
     personagemSelec.classList.remove("selecionado")
     personagem.classList.add("selecionado")
     personagemGGIMG.src = link
